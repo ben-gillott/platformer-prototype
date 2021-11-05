28e6f657e6119eef57d10463f92929cdb8f78524
Benjamin Gillott <bg357@cornell.edu>
2021-11-04 22:12:08 -0800

    progress again

---
+++ GameOne/Coin.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	position.y = 20
+	print("started")
+	pass # Replace with function body.
+
+var gravityVal = 100
+var velocityVal = 50
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	position.y = position.y + velocityVal * delta
+	velocityVal += gravityVal * delta
+	
+	if(position.y > 900):
+		queue_free()
+	pass
--- GameOne/CoinManager.gd
+++ GameOne/CoinManager.gd
@@ -1,23 +1,37 @@
 extends Node2D
 
-onready var coin = $Coin
+#onready var coin = $Coin
 var score = 0
+
+var coin = preload("res://Coin.tscn")
 
 
 func _spawnCoin():
-	var coinClone = coin.new()
-	add_child(coinClone)
-	#TODO, change to its own scene and duplicate that
+	#var coinClone = Area2D.new()
+	#add_child(coinClone)
+	var coin_instance = coin.instance()
+	#get_tree().get_root().get_node("Game").add_child(coin_instance)
+	coin_instance.position = Vector2(510.0, 10.0)
+	call_deferred("add_child", coin_instance)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	_spawnCoin()
+	pass
+
+var timer = 0; 
+var spawnRate = 1;
 
 func _process(delta):
-	pass
+	timer += delta
+	if timer > spawnRate:
+		timer = 0
+		print("time hit")
 
 #When the player hits a coin
 func _on_Player_area_entered(area):
-	if area.name == "Coin":
+	#if area.name == "Coin":
 		score += 1
 		print(score)
+		area.free()
+		#_spawnCoin()
